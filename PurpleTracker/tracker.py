ec5a9b7edcf5a6cea332a87b43ef98d917580fac
Deity <ddeityy@gmail.com>
2022-04-29 02:32:46 +0300

    fix

--- PurpleTracker/tracker.py
+++ PurpleTracker/tracker.py
@@ -20,12 +20,15 @@ while True:
     conts, hier = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     if len(conts) != 0:
         for cont in conts:
-            if 1000 > cv.contourArea(cont) > 250:
-                M = cv.moments(cont)
-                if M['m00'] != 0:
-                    cx = int(M['m10']/M['m00'])
-                    cy = int(M['m01']/M['m00'])
-                    frames.write(f"txt[{counter}] = [{cx}, {cy}];\n")
-                    frames.flush()
+            M = cv.moments(cont)
+            if M['m00'] != 0:
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                frames.write(f"txt[{counter}] = [{cx}, {cy}];\n")
+                frames.flush()
+    else:
+        frames.write(f"txt[{counter}] = [3000, 3000];\n")
+        frames.flush()
+        
     cv.waitKey(1)
     
