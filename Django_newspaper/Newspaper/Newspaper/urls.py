b9684fb6bc26d11e45d542b6fb2c4218592284c8
Deity <ddeityy@gmail.com>
2022-05-03 07:46:35 +0300

    .

--- Django_newspaper/Newspaper/Newspaper/urls.py
+++ Django_newspaper/Newspaper/Newspaper/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url="/news/")),
     path('admin/', admin.site.urls),
     path('news/', include('NewsPortal.urls')),
 ]
